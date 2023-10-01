edcfef54c95bf727b247b4b6aeec0251e42f619a
owo-del <123456789+trowom001@protonmail.com>
2023-10-01 10:26:18 +0800

    add next and previous buttons interaction

---
+++ script.js
@@ -0,0 +1,30 @@
+const slides = document.querySelectorAll(`.slides`);
+const next = document.querySelector(`.next`);
+const prev = document.querySelector(`.previous`);
+let i = 0;
+
+function activeSlides(slide){
+    for (const slide of slides){
+        slide.classList.remove(`default`);
+    }
+    slides[slide].classList.add(`default`);
+};
+
+next.addEventListener(`click`, () => {
+    if(i === slides.length - 1){
+        i = 0;
+    } else {
+        i++;
+    }
+    activeSlides(i);
+});
+
+
+prev.addEventListener(`click`, () => {
+    if(i === 0){
+        i = slides.length - 1;
+    } else {
+        i--;
+    }
+    activeSlides(i);
+});
